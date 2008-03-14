1426e9607340c8374bb81750e95238141a710ccd
jcofflan <jcofflan@ce46b6ef-3445-0410-911d-c47028e6f9df>
2008-03-14 20:41:25 +0000

    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1038 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/config.py
+++ protomol/config.py
@@ -54,43 +54,43 @@ if env['CC'] == 'gcc':
 
 
 # LAPACK
-if os.environ.has_key('LAPACK_HOME'):
-    env.Append(CPPPATH = [os.environ['LAPACK_HOME']])
-    env.Append(LIBPATH = [os.environ['LAPACK_HOME']])
+if globals().has_key('use_lapack') and use_lapack:
+    if os.environ.has_key('LAPACK_HOME'):
+        env.Append(CPPPATH = [os.environ['LAPACK_HOME']])
+        env.Append(LIBPATH = [os.environ['LAPACK_HOME']])
   
-have_lapack = 0
-if conf.CheckLib('lapack'):
-    env.Append(CPPDEFINES = ['HAVE_LAPACK'])
-    have_lapack = 1
+    have_lapack = 0
+    if conf.CheckLib('lapack'):
+        env.Append(CPPDEFINES = ['HAVE_LAPACK'])
+        have_lapack = 1
 
 
-if env['CC'] == 'gcc' and have_lapack:
-    # BLAS
-    if os.environ.has_key('BLAS_HOME'):
-        env.Append(LIBPATH = [os.environ['BLAS_HOME']])
+    if env['CC'] == 'gcc' and have_lapack:
+        # BLAS
+        if os.environ.has_key('BLAS_HOME'):
+            env.Append(LIBPATH = [os.environ['BLAS_HOME']])
 
-    have_blas = 0  
-    if conf.CheckLib('blas'):
-        have_blas = 1
-
-
-    # G2C
-    if os.environ.has_key('G2C_HOME'):
-        env.Append(LIBPATH = [os.environ['G2C_HOME']])
-
-    have_g2c = 0  
-    if conf.CheckLib('g2c'):
-        have_g2c = 1
+        have_blas = 0  
+        if conf.CheckLib('blas'):
+            have_blas = 1
 
 
-# SimTK LAPACK
-if os.environ.has_key('SIMTK_LAPACK_HOME'):
-    env.Append(CPPPATH = [os.environ['SIMTK_LAPACK_HOME'] + '/include'])
-    env.Append(LIBPATH = [os.environ['SIMTK_LAPACK_HOME'] + '/lib'])
+        # G2C
+        if os.environ.has_key('G2C_HOME'):
+            env.Append(LIBPATH = [os.environ['G2C_HOME']])
+
+        have_g2c = 0  
+        if conf.CheckLib('g2c'):
+            have_g2c = 1
+
+
+if globals().has_key('use_simtk_lapack') and use_simtk_lapack:
+    # SimTK LAPACK
+    if os.environ.has_key('SIMTK_LAPACK_HOME'):
+        env.Append(CPPPATH = [os.environ['SIMTK_LAPACK_HOME'] + '/include'])
+        env.Append(LIBPATH = [os.environ['SIMTK_LAPACK_HOME'] + '/lib'])
   
-have_simtk_lapack = 0
-if conf.CheckLib('SimTKlapack') and conf.CheckCXXHeader('SimTKlapack.h'):
-    env.Append(CPPDEFINES = ['HAVE_SIMTK_LAPACK'])
-    have_simtk_lapack = 1
-
-
+    have_simtk_lapack = 0
+    if conf.CheckLib('SimTKlapack') and conf.CheckCXXHeader('SimTKlapack.h'):
+        env.Append(CPPDEFINES = ['HAVE_SIMTK_LAPACK'])
+        have_simtk_lapack = 1
