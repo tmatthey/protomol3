0d3a6bc644e61f10d8d7c204013eaaa7c4b8c778
chrissweet <chrissweet@ce46b6ef-3445-0410-911d-c47028e6f9df>
2008-06-13 01:23:17 +0000

    Removed AutoCorrelatorOuter
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1235 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/protomol/module/LeapfrogModule.cpp
+++ protomol/protomol/module/LeapfrogModule.cpp
@@ -9,7 +9,6 @@
 #include <protomol/integrator/leapfrog/NoseNVTLeapfrogIntegrator.h>
 
 // Autocorrelators are added here because they run Leapfrog internally
-#include <protomol/integrator/autocorrelator/AutoCorrelatorOuter.h>
 #include <protomol/integrator/autocorrelator/AutoCorrelatorInner.h>
 
 using namespace std;
@@ -21,6 +20,5 @@ void LeapfrogModule::init(ProtoMolApp *app) {
   app->integratorFactory.registerExemplar(new DMDLeapfrogIntegrator());
   app->integratorFactory.registerExemplar(new PLeapfrogIntegrator());
   app->integratorFactory.registerExemplar(new NoseNVTLeapfrogIntegrator());
-  app->integratorFactory.registerExemplar(new AutoCorrelatorOuter());
   app->integratorFactory.registerExemplar(new AutoCorrelatorInner());
 }
