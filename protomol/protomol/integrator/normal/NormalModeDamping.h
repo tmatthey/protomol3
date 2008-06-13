8e3685b5a75149a394c8c2c08c8c8e4b3b10c8d7
chrissweet <chrissweet@ce46b6ef-3445-0410-911d-c47028e6f9df>
2008-06-13 01:19:31 +0000

    NM damping, fixed tabs to spaces
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1234 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/protomol/integrator/normal/NormalModeDamping.h
+++ protomol/protomol/integrator/normal/NormalModeDamping.h
@@ -11,80 +11,80 @@
 
 namespace ProtoMol {
 
-	class ScalarStructure;
-	class ForceGroup;
+    class ScalarStructure;
+    class ForceGroup;
 
-	//__________________________________________________ NormalModeDamping
-	class NormalModeDamping : public STSIntegrator, public NormalModeUtilities {
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// Constructors, destructors, assignment
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	public:
-		NormalModeDamping();
-		NormalModeDamping(Real timestep, int firstmode, int nummode,
-			std::string dcdf, std::string modeff, std::string atomff, Real temp,
-			ForceGroup *overloadedForces);
-		~NormalModeDamping(); 
+    //__________________________________________________ NormalModeDamping
+    class NormalModeDamping : public STSIntegrator, public NormalModeUtilities {
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Constructors, destructors, assignment
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public:
+        NormalModeDamping();
+        NormalModeDamping(Real timestep, int firstmode, int nummode,
+            std::string dcdf, std::string modeff, std::string atomff, Real temp,
+            ForceGroup *overloadedForces);
+        ~NormalModeDamping(); 
 
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// New methods of class NormalModeDamping
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	protected:
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// From class Makeable
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	public:
-		virtual std::string getIdNoAlias() const{return keyword;}
-		virtual unsigned int getParameterSize() const{return 7;}
-		virtual void getParameters(std::vector<Parameter>& parameters) const;
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // New methods of class NormalModeDamping
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    protected:
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // From class Makeable
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public:
+        virtual std::string getIdNoAlias() const{return keyword;}
+        virtual unsigned int getParameterSize() const{return 7;}
+        virtual void getParameters(std::vector<Parameter>& parameters) const;
 
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// From class Integrator
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	public:
-		virtual void initialize(ProtoMolApp* appp);
-		virtual void run(int numTimesteps);
-	protected:
-		//virtual void addModifierAfterInitialize();
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // From class Integrator
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public:
+        virtual void initialize(ProtoMolApp* appp);
+        virtual void run(int numTimesteps);
+    protected:
+        //virtual void addModifierAfterInitialize();
 
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// From class STSIntegrator
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	private:
-		virtual STSIntegrator* doMake(const std::vector<Value>& values, ForceGroup* fg)const;
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // From class STSIntegrator
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    private:
+        virtual STSIntegrator* doMake(const std::vector<Value>& values, ForceGroup* fg)const;
 
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// New methods of class NormalModeDamping
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	public:
-		//virtual void forceProjection();
-	private:	
-		void modeProjector(Vector3DBlock *inp, double *tmpc, bool forcep);
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // New methods of class NormalModeDamping
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public:
+        //virtual void forceProjection();
+    private:	
+        void modeProjector(Vector3DBlock *inp, double *tmpc, bool forcep);
 
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// My data members
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	public:
-		static const std::string keyword;
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // My data members
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public:
+        static const std::string keyword;
 
-	private:
-		Real randStp, myTemperature;
-		int aveForceCount, counter;
-		// dcd input and x0
-		std::string myDCDFile;
-		// mode force output:
-		std::string modeForceFile, atomForceFile; 
+    private:
+        Real randStp, myTemperature;
+        int aveForceCount, counter;
+        // dcd input and x0
+        std::string myDCDFile;
+        // mode force output:
+        std::string modeForceFile, atomForceFile; 
 
-		DCDTrajectoryReader myReaderF, myReaderX, myReaderV;
-		Vector3DBlock *waterForces, x0;
+        DCDTrajectoryReader myReaderF, myReaderX, myReaderV;
+        Vector3DBlock *waterForces, x0;
 
-		XYZTrajectoryWriter *myWriter;
+        XYZTrajectoryWriter *myWriter;
 
-		double *posC, *velC;
-		
-		ofstream myFile;
+        double *posC, *velC;
+        
+        ofstream myFile;
 
-	};
+    };
 }
 
 #endif
