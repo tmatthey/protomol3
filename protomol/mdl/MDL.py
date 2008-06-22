f4323174c3d4a7261fa7aacada397c31105763d6
tcickovs <tcickovs@ce46b6ef-3445-0410-911d-c47028e6f9df>
2008-06-22 15:54:12 +0000

    Added module imports for Windows.
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1278 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/mdl/MDL.py
+++ protomol/mdl/MDL.py
@@ -6,40 +6,70 @@ numpydir = pythondir
 gnuplotdir = pythondir
 pyversion = 'python2.5'
 
-pwd = os.getenv('PWD')
+if (sys.platform == 'win32'):
+   pwd = os.getenv('PYTHONPATH')
+else:
+   pwd = os.getenv('PWD')
 
 sys.path.append(pwd)
-sys.path.append(pwd+'/interface/')
-sys.path.append(pwd+'/interface/integrator')
-sys.path.append(pwd+'/interface/integrator/base/')
-sys.path.append(pwd+'/interface/integrator/hessian/')
-sys.path.append(pwd+'/interface/integrator/normal/')
-sys.path.append(pwd+'/interface/integrator/leapfrog/')
-sys.path.append(pwd+'/interface/topology/')
-sys.path.append(pwd+'/interface/io/')
-sys.path.append(pwd+'/interface/output/')
-sys.path.append(pwd+'/interface/force/')
-sys.path.append(pwd+'/interface/force/system/')
-sys.path.append(pwd+'/interface/force/bonded/')
-sys.path.append(pwd+'/interface/force/nonbonded/')
-sys.path.append(pwd+'/interface/type/')
-sys.path.append(pwd+'/interface/base/')
-sys.path.append(pwd+'/src/')
-sys.path.append(pwd+'/src/factories/')
-sys.path.append(pwd+'/src/modifiers/')
-sys.path.append(pwd+'/src/propagators/')
-sys.path.append(pwd+'/src/propagators/objects/')
-sys.path.append(pwd+'/src/toplevel')
-sys.path.append(gnuplotdir+'/lib/'+pyversion+'/site-packages/Gnuplot/')
-sys.path.append(numpydir+'/lib/'+pyversion+'/site-packages/')
-sys.path.append(numpydir+'/lib/'+pyversion+'/site-packages/numpy/')
+if (sys.platform == 'win32'):
+ sys.path.append(pwd+'/interface')
+ sys.path.append(pwd+'/interface/integrator')
+ sys.path.append(pwd+'/interface/integrator/base')
+ sys.path.append(pwd+'/interface/integrator/hessian')
+ sys.path.append(pwd+'/interface/integrator/normal')
+ sys.path.append(pwd+'/interface/integrator/leapfrog')
+ sys.path.append(pwd+'/interface/topology')
+ sys.path.append(pwd+'/interface/io')
+ sys.path.append(pwd+'/interface/output')
+ sys.path.append(pwd+'/interface/force')
+ sys.path.append(pwd+'/interface/force/system')
+ sys.path.append(pwd+'/interface/force/bonded')
+ sys.path.append(pwd+'/interface/force/nonbonded')
+ sys.path.append(pwd+'/interface/type')
+ sys.path.append(pwd+'/interface/base')
+ sys.path.append(pwd+'/src')
+ sys.path.append(pwd+'/src/factories')
+ sys.path.append(pwd+'/src/modifiers')
+ sys.path.append(pwd+'/src/propagators')
+ sys.path.append(pwd+'/src/propagators/objects')
+ sys.path.append(pwd+'/src/toplevel')
+ sys.path.append(gnuplotdir+'/Lib/site-packages/Gnuplot')
+ sys.path.append(numpydir+'/Lib/site-packages')
+ sys.path.append(numpydir+'/Lib/site-packages/numpy')
+ sys.path.append(numpydir+'/Lib/site-packages/matplotlib')
+else:
+ sys.path.append(pwd+'/interface/')
+ sys.path.append(pwd+'/interface/integrator')
+ sys.path.append(pwd+'/interface/integrator/base/')
+ sys.path.append(pwd+'/interface/integrator/hessian/')
+ sys.path.append(pwd+'/interface/integrator/normal/')
+ sys.path.append(pwd+'/interface/integrator/leapfrog/')
+ sys.path.append(pwd+'/interface/topology/')
+ sys.path.append(pwd+'/interface/io/')
+ sys.path.append(pwd+'/interface/output/')
+ sys.path.append(pwd+'/interface/force/')
+ sys.path.append(pwd+'/interface/force/system/')
+ sys.path.append(pwd+'/interface/force/bonded/')
+ sys.path.append(pwd+'/interface/force/nonbonded/')
+ sys.path.append(pwd+'/interface/type/')
+ sys.path.append(pwd+'/interface/base/')
+ sys.path.append(pwd+'/src/')
+ sys.path.append(pwd+'/src/factories/')
+ sys.path.append(pwd+'/src/modifiers/')
+ sys.path.append(pwd+'/src/propagators/')
+ sys.path.append(pwd+'/src/propagators/objects/')
+ sys.path.append(pwd+'/src/toplevel')
+ sys.path.append(gnuplotdir+'/lib/'+pyversion+'/site-packages/Gnuplot/')
+ sys.path.append(numpydir+'/lib/'+pyversion+'/site-packages/')
+ sys.path.append(numpydir+'/lib/'+pyversion+'/site-packages/numpy/')
 
 
 os.environ['MDLROOT'] = pwd
 os.system('export LD_LIBRARY_PATH='+pythondir+'/lib/'+pyversion+':$LD_LIBRARY_PATH')
 
 if (not os.environ.has_key('PROTOMOL_HOME')):
-    os.environ['PROTOMOL_HOME'] = pwd+':/../'
+    os.environ['PROTOMOL_HOME'] = pwd+'/../'
 
 
 class Prompt:
