23b84391a20cfd59b9add31a849c4b14d5d6853e
chrissweet <chrissweet@ce46b6ef-3445-0410-911d-c47028e6f9df>
2009-01-24 17:56:06 +0000

    Updated SCPISM paramiters table to only require data from Hassan's input file. A_i, B_i and C_i are now calculated from the base atom type, atom radius from the LJ paramiters and square roots explicitly calculated. This is consistant with the CHARMM implementation.
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1357 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/protomol/topology/CoulombSCPISMParameterTable.cpp
+++ protomol/protomol/topology/CoulombSCPISMParameterTable.cpp
@@ -9,208 +9,207 @@ using namespace ProtoMol;
 void CoulombSCPISMParameterTable::populateTable() {
   CoulombSCPISMParameters temp;
   // Order:
-  // alpha_i, hbond_factor, R_iw, sqrt_alpha_i, r_cov, gamma_i, isHbonded, A_i,
-  // B_i, C_i, R_vdw
-  
-  temp.set(0.4906, 0.6259, 0.5000, 0.7004, 0.3700, 0.0052, PH, 1.20,
-           3.68, 0.80, 0.224500);
-  myData["H"] = temp; // polar H
-  temp.set(0.5300, 9.6746, 0.5000, 0.7280, 0.3700, 0.0052, PH, 1.20,
-           3.68, 0.80, 0.224500);
-  myData["HC"] = temp; // N-ter H
-  temp.set(0.5300, 0.5000, 0.5000, 0.7280, 0.3700, 0.0052, NO, 17.0, 
-           9.00, 0.50, 1.320000);
-  myData["HA"] = temp; // nonpolar H
-  temp.set(0.4906, 2.3800, 0.5000, 0.7004, 0.3700, 0.0052, PH, 1.20, 
-           3.68, 0.80, 0.224500);
-  myData["HT"] = temp; // TIPS3P Water H
-  temp.set(0.5274, 0.5000, 0.5000, 0.7262, 0.3700, 0.0052, NO, 17.0,  
-           9.00, 0.50, 1.358200);
-  myData["HP"] = temp; // aromatic H
-  temp.set(0.4858, 0.5000, 0.5000, 0.6970, 0.3700, 0.0052, NO, 17.0,  
-           9.00, 0.50, 1.320000);
-  myData["HB"] = temp; // backbone H
-  temp.set(0.4651, 0.5000, 0.5000, 0.6820, 0.3700, 0.0052, NO, 17.0,  
-           9.00, 0.50, 0.900000);
-  myData["HR1"] = temp; //his he1, (+) his HG,HD2
-  temp.set(0.4580, 0.5000, 0.5000, 0.6768, 0.3700, 0.0052, NO, 17.0,  
-           9.00, 0.50, 0.700000);
-  myData["HR2"] = temp; //(+) his HE1
-  temp.set(0.4893, 0.5000, 0.5000, 0.6995, 0.3700, 0.0052, NO, 17.0,  
-           9.00, 0.50, 1.468000);
-  myData["HR3"] = temp; //neutral his HG, HD2
-  temp.set(0.4859, 0.5000, 0.5000, 0.6971, 0.3700, 0.0052, NO, 17.0,  
-           9.00, 0.50, 0.450000);
-  myData["HS"] = temp; // thiol H
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.3700, 0.0052, NO, 17.0,  
-           9.00, 0.50, 0.224500);
-  myData["HA1"] = temp; // for alkene; RHC=CR, wildcard Rvdw
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.3700, 0.0052, NO, 17.0,  
-           9.00, 0.50, 0.224500);
-  myData["HA2"] = temp; // for alkene; H2C=CR, wildcard Rvdw
-  temp.set(0.5298, 0.5000, 0.5000, 0.7273, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.000000);
-  myData["C"] = temp; // polar C
-  temp.set(0.5192, 0.5000, 0.5000, 0.7206, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 1.992400);
-  myData["CA"] = temp; // aromatic C
-  temp.set(0.5209, 0.5000, 0.5000, 0.7217, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.275000);
-  myData["CT1"] = temp; //aliphatic sp3 C for CH
-  temp.set(0.5298, 0.5000, 0.5000, 0.7273, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.175000);
-  myData["CT2"] = temp; //aliphatic sp3 C for CH2
-  temp.set(0.5082, 0.5000, 0.5000, 0.7129, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.060000);
-  myData["CT3"] = temp; //aliphatic sp3 C for CH3
-  temp.set(0.4538, 0.5000, 0.5000, 0.6736, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 1.800000);
-  myData["CPH1"] = temp; //his CG and CD2 carbons
-  temp.set(0.4974, 0.5000, 0.5000, 0.7053, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 1.800000);
-  myData["CPH2"] = temp; //his CE1 carbon
-  temp.set(0.5217, 0.5000, 0.5000, 0.7229, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 1.800000);
-  myData["CPT"] = temp; //trp C between rings
-  temp.set(0.5240, 0.5000, 0.5000, 0.7239, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 1.992400);
-  myData["CY"] = temp; //TRP C in pyrrole ring
-  temp.set(0.4763, 0.5000, 0.5000, 0.6901, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.275000);
-  myData["CP1"] = temp; //tetrahedral C (proline CA)
-  temp.set(0.4599, 0.5000, 0.5000, 0.6782, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.175000);
-  myData["CP2"] = temp; //tetrahedral C (proline CB/CG)
-  temp.set(0.4627, 0.5000, 0.5000, 0.6802, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.175000);
-  myData["CP3"] = temp; //tetrahedral C (proline CD)
-  temp.set(0.4970, 0.5000, 0.5000, 0.7050, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.000000);
-  myData["CC"] = temp; //carbonyl C for asn,asp,gln,glu
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.000000);
-  myData["CD"] = temp; //carbonyl C for none amides, asp,glu,cter
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 1.800000);
-  myData["CPA"] = temp; //heme alpha-C
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 1.800000);
-  myData["CPB"] = temp; //heme beta-C
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 1.800000);
-  myData["CPM"] = temp; //heme meso-C
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.100000);
-  myData["CM"] = temp; //heme CO carbon
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.200000);
-  myData["CS"] = temp; //thiolate carbon
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.090000);
-  myData["CE1"] = temp; //for alkene; RHC=CR
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO, 31.0,  
-           7.92, 0.32, 2.080000);
-  myData["CE2"] = temp; //for alkene; H2C=CR
-  temp.set(0.4894, 0.5000, 0.5000, 0.6996, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["N"] = temp; // proline N
-  temp.set(0.4598, 0.5000, 0.5000, 0.6781, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NR1"] = temp; // neutral his protonated ring N
-  temp.set(0.4839, 19.4824, 0.6956, 0.5000, 0.7400, 0.0052, PA, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NR2"] = temp; // neutral his unprotonated ring N
-  temp.set(0.4742, 0.5000, 0.5000, 0.6886, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NR3"] = temp; // charged his  ring N
-  temp.set(0.5103, 0.5000, 0.5000, 0.7144, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NH1"] = temp; // peptide N
-  temp.set(0.5045, 0.5000, 0.5000, 0.7103, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NH2"] = temp; // amide N
-  temp.set(0.5300, 0.5000, 0.5000, 0.7280, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NH3"] = temp; // ammonium N
-  temp.set(0.5299, 0.5000, 0.5000, 0.7279, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NC2"] = temp; // guanidinium N
-  temp.set(0.5284, 0.5000, 0.5000, 0.7269, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NY"] = temp; // TRP N in pyrrole ring
-  temp.set(0.4894, 0.5000, 0.5000, 0.6996, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NP"] = temp; // Proline ring NH2+ (N-terminal)
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO, 60.0,  
-           9.66, 0.22, 1.850000);
-  myData["NPH"] = temp; //heme pyrrole N
-  temp.set(0.4810, 4.0085, 0.5000, 0.6935, 0.7400, 0.0052, PA, 40.0,  
-           8.52, 0.29, 1.700000);
-  myData["O"] = temp; // carbonyl oxygen
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO, 40.0,  
-           8.52, 0.29, 1.700000);
-  myData["OB"] = temp; //carbonyl oxygen in acetic acid
-  temp.set(0.6000, 8.1838, 0.5000, 0.7746, 0.7400, 0.0052, PA, 40.0,  
-           8.52, 0.29, 1.700000);
-  myData["OC"] = temp; // carboxlate oxygen
-  temp.set(0.4843, 3.3433, 0.5000, 0.6959, 0.7400, 0.0052, PA, 40.0,  
-           8.52, 0.29, 1.770000);
-  myData["OH1"] = temp; // hydroxyl oxygen
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO, 40.0,  
-           8.52, 0.29, 1.770000);
-  myData["OS"] = temp; // ester oxygen
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO, 40.0,  
-           8.52, 0.29, 1.768200);
-  myData["OT"] = temp; // TIPS3P Water O
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO, 40.0,  
-           8.52, 0.29, 1.700000);
-  myData["OM"] = temp; // heme CO/O2 oxygen
-  temp.set(0.5054, 5.0037, 0.5000, 0.7109, 1.0400, 0.0052, PA, 60.0,  
-           9.10, 0.22, 2.000000);
-  myData["S"] = temp; // sulphur
-  temp.set(0.5054, 0.5000, 0.5000, 0.7109, 1.0400, 0.0052, NO, 60.0,  
-           9.10, 0.22, 1.975000);
-  myData["SM"] = temp; // sulphur C-S-S-C type
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 1.0400, 0.0052, NO, 60.0,  
-           9.10, 0.22, 2.200000);
-  myData["SS"] = temp; // thiolate sulphur
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO, 0.0,  
-           0.00, 0.00, 1.4800);
-  myData["HE"] = temp; // helium
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO, 0.0,  
-           0.00, 0.00, 1.5300);
-  myData["NE"] = temp; // neon
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO, 0.0,  
-           0.00, 0.00, 1.3670);
-  myData["CAL"] = temp; // calcium 2+
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO, 0.0,  
-           0.00, 0.00, 1.0900);
-  myData["ZN"] = temp; // zinc (II) cation
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO, 0.0,  
-           0.00, 0.00, 0.6500);
-  myData["FE"] = temp; // heme iron 56
-  temp.set(0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO, 0.0,  
-           0.00, 0.00, 0.0000);
-  myData["DUM"] = temp; // dummy atom
+  // name, alpha_i, hbond_factor, R_iw, r_cov, gamma_i, isHbonded
+
+  std::string atom_name;
+
+  atom_name = "H"; 
+  temp.set(atom_name, 0.4906, 0.6259, 0.5000, 0.3700, 0.0052, PH);
+  myData[atom_name] = temp; // polar H
+  atom_name = "HC"; 
+  temp.set(atom_name, 0.5300, 9.6746, 0.5000, 0.3700, 0.0052, PH);
+  myData[atom_name] = temp; // N-ter H
+  atom_name = "HA"; 
+  temp.set(atom_name, 0.5300, 0.5000, 0.5000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; // nonpolar H
+  atom_name = "HT"; 
+  temp.set(atom_name, 0.4906, 2.3800, 0.5000, 0.3700, 0.0052, PH);
+  myData[atom_name] = temp; // TIPS3P Water H
+  atom_name = "HP"; 
+  temp.set(atom_name, 0.5274, 0.5000, 0.5000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; // aromatic H
+  atom_name = "HB"; 
+  temp.set(atom_name, 0.4858, 0.5000, 0.5000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; // backbone H
+  atom_name = "HR1"; 
+  temp.set(atom_name, 0.4651, 0.5000, 0.5000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; //his he1, (+) his HG,HD2
+  atom_name = "HR2"; 
+  temp.set(atom_name, 0.4580, 0.5000, 0.5000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; //(+) his HE1
+  atom_name = "HR3"; 
+  temp.set(atom_name, 0.4893, 0.5000, 0.5000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; //neutral his HG, HD2
+  atom_name = "HS"; 
+  temp.set(atom_name, 0.4859, 0.5000, 0.5000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; // thiol H
+  atom_name = "HA1"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; // for alkene; RHC=CR, wildcard Rvdw
+  atom_name = "HA2"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.3700, 0.0052, NO);
+  myData[atom_name] = temp; // for alkene; H2C=CR, wildcard Rvdw
+  atom_name = "C"; 
+  temp.set(atom_name, 0.5298, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; // polar C
+  atom_name = "CA"; 
+  temp.set(atom_name, 0.5192, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; // aromatic C
+  atom_name = "CT1"; 
+  temp.set(atom_name, 0.5209, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //aliphatic sp3 C for CH
+  atom_name = "CT2"; 
+  temp.set(atom_name, 0.5298, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //aliphatic sp3 C for CH2
+  atom_name = "CT3"; 
+  temp.set(atom_name, 0.5082, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //aliphatic sp3 C for CH3
+  atom_name = "CPH1"; 
+  temp.set(atom_name, 0.4538, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //his CG and CD2 carbons
+  atom_name = "CPH2"; 
+  temp.set(atom_name, 0.4974, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //his CE1 carbon
+  atom_name = "CPT"; 
+  temp.set(atom_name, 0.5217, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //trp C between rings
+  atom_name = "CY"; 
+  temp.set(atom_name, 0.5240, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //TRP C in pyrrole ring
+  atom_name = "CP1"; 
+  temp.set(atom_name, 0.4763, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //tetrahedral C (proline CA)
+  atom_name = "CP2"; 
+  temp.set(atom_name, 0.4599, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //tetrahedral C (proline CB/CG)
+  atom_name = "CP3"; 
+  temp.set(atom_name, 0.4627, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //tetrahedral C (proline CD)
+  atom_name = "CC"; 
+  temp.set(atom_name, 0.4970, 0.5000, 0.5000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //carbonyl C for asn,asp,gln,glu
+  atom_name = "CD"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //carbonyl C for none amides, asp,glu,cter
+  atom_name = "CPA"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //heme alpha-C
+  atom_name = "CPB"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //heme beta-C
+  atom_name = "CPM"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //heme meso-C
+  atom_name = "CM"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //heme CO carbon
+  atom_name = "CS"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //thiolate carbon
+  atom_name = "CE1"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //for alkene; RHC=CR
+  atom_name = "CE2"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7700, 0.0052, NO);
+  myData[atom_name] = temp; //for alkene; H2C=CR
+  atom_name = "N"; 
+  temp.set(atom_name, 0.4894, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // proline N
+  atom_name = "NR1"; 
+  temp.set(atom_name, 0.4598, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // neutral his protonated ring N
+  atom_name = "NR2"; 
+  temp.set(atom_name, 0.4839, 19.4824, 0.6956,0.7400, 0.0052, PA);
+  myData[atom_name] = temp; // neutral his unprotonated ring N
+  atom_name = "NR3"; 
+  temp.set(atom_name, 0.4742, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // charged his  ring N
+  atom_name = "NH1"; 
+  temp.set(atom_name, 0.5103, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // peptide N
+  atom_name = "NH2"; 
+  temp.set(atom_name, 0.5045, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // amide N
+  atom_name = "NH3"; 
+  temp.set(atom_name, 0.5300, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // ammonium N
+  atom_name = "NC2"; 
+  temp.set(atom_name, 0.5299, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // guanidinium N
+  atom_name = "NY"; 
+  temp.set(atom_name, 0.5284, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // TRP N in pyrrole ring
+  atom_name = "NP"; 
+  temp.set(atom_name, 0.4894, 0.5000, 0.5000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // Proline ring NH2+ (N-terminal)
+  atom_name = "NPH"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; //heme pyrrole N
+  atom_name = "O"; 
+  temp.set(atom_name, 0.4810, 4.0085, 0.5000, 0.7400, 0.0052, PA);
+  myData[atom_name] = temp; // carbonyl oxygen
+  atom_name = "OB"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; //carbonyl oxygen in acetic acid
+  atom_name = "OC"; 
+  temp.set(atom_name, 0.6000, 8.1838, 0.5000, 0.7400, 0.0052, PA);
+  myData[atom_name] = temp; // carboxlate oxygen
+  atom_name = "OH1"; 
+  temp.set(atom_name, 0.4843, 3.3433, 0.5000, 0.7400, 0.0052, PA);
+  myData[atom_name] = temp; // hydroxyl oxygen
+  atom_name = "OS"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // ester oxygen
+  atom_name = "OT"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // TIPS3P Water O
+  atom_name = "OM"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.7400, 0.0052, NO);
+  myData[atom_name] = temp; // heme CO/O2 oxygen
+  atom_name = "S"; 
+  temp.set(atom_name, 0.5054, 5.0037, 0.5000, 1.0400, 0.0052, PA);
+  myData[atom_name] = temp; // sulphur
+  atom_name = "SM"; 
+  temp.set(atom_name, 0.5054, 0.5000, 0.5000, 1.0400, 0.0052, NO);
+  myData[atom_name] = temp; // sulphur C-S-S-C type
+  atom_name = "SS"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 1.0400, 0.0052, NO);
+  myData[atom_name] = temp; // thiolate sulphur
+  atom_name = "HE"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO);
+  myData[atom_name] = temp; // helium
+  atom_name = "NE"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO);
+  myData[atom_name] = temp; // neon
+  atom_name = "CAL"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO);
+  myData[atom_name] = temp; // calcium 2+
+  atom_name = "ZN"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO);
+  myData[atom_name] = temp; // zinc (II) cation
+  atom_name = "FE"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO);
+  myData[atom_name] = temp; // heme iron 56
+  atom_name = "DUM"; 
+  temp.set(atom_name, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, NO);
+  myData[atom_name] = temp; // dummy atom
 }
 
 void CoulombSCPISMParameterTable::displayTable() {
-  report << "Coulomb SCPISM Parameter Table!!!" << endl;
+  report << debug(2) << "Coulomb SCPISM Parameter Table." << endl;
   
   map<string, CoulombSCPISMParameters>::const_iterator it;
   for (it = myData.begin(); it != myData.end(); it++) {
-    report << it->first << "\t";
+    report << debug(2) << it->first << "\t";
     report << it->second.alpha_i << "\t";
     report << it->second.hbond_factor << "\t";
     report << it->second.R_iw << "\t";
-    //report << it->second.sqrt_alpha_i << "\t";
     report << it->second.r_cov << "\t";
     report << it->second.gamma_i << "\t";
     report << it->second.isHbonded << "\t";
     report << it->second.A_i << "\t";
     report << it->second.B_i << "\t";
     report << it->second.C_i << "\t";
-    //report << it->second.R_vdw << "\t";
     report << endl;
   }
 }
