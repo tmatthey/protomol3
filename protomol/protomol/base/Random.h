d2222d6cc743a385615326654a70da541627f854
chrissweet <chrissweet@ce46b6ef-3445-0410-911d-c47028e6f9df>
2009-02-16 04:35:59 +0000

    Added Random, a Lewis,Goodman,Miller 'minimal' RNG with Schrage's algorithm and Bays-Durham shuffle. This has a save /restore to stream for system restarts. 1e7 generations show bin errors of 7.45e-5 on 100 bins (0.01) compared to 8.4e-5 for rand(). Decreases to 2.38e-5 at 1e8 and 5.82e-6 at 1e9.
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1370 ce46b6ef-3445-0410-911d-c47028e6f9df

---
+++ protomol/protomol/base/Random.h
@@ -0,0 +1,66 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <protomol/type/Real.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+namespace ProtoMol {
+
+  //_____________________________________________________________ Random
+  /**
+   * Random number generation
+   * "Minimal" random number generator of Park and Miller with Bays-Durham shuffle and added
+   * safeguards. Returns a uniform random deviate between 0.0 and 1.0 (exclusive of the endpoint
+   * values). Call with intermediate_rand a negative integer to initialize; thereafter, do not alter intermediate_rand between
+   * successive deviates in a sequence. LESS_THAN_ONE should approximate the largest floating value that is
+   * less than 1.
+   */
+  class Random {
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Types & enum's
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  private:
+    enum {SEED_DEFAULT = 1234};
+    enum {MULTIPLIER = 16807};
+    enum {MUDULUS = 2147483647};
+    enum {SCHRAGE_Q = 127773};
+    enum {SCHRAGE_R = 2836};
+    enum {SHUFFLE_LEN_VAL = 32};
+    enum {SHUFFLE_DIVISOR = (1+(MUDULUS-1)/SHUFFLE_LEN_VAL)};
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Constructors, destructors, assignment
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public:
+
+    Random();
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //  New methods of class Random
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public:
+    void seed( int seed );
+    double rand();
+    friend ostream &operator<<(ostream &stream, Random ob);
+    friend istream &operator>>(istream &stream, Random &ob);
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // My data members
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public:
+
+  private:
+    int intermediate_rand;
+    std::vector<int> shuffle_array;
+    int shuffle_val;
+    static const int SHUFFLE_LEN;
+    static const double EPSILON, INVERSE_MODULUS, LESS_THAN_ONE;
+    
+  };
+
+};
+
+#endif // RANDOM_H
