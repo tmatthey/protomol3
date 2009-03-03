dc8a961146b6faa00ea0897dee36de9eb4166e54
omegaice <omegaice@ce46b6ef-3445-0410-911d-c47028e6f9df>
2009-03-03 18:52:28 +0000

    Fixed so that the output rate is set to the global output rate if the user does not provide one.
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1428 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/protomol/output/OutputCheckpoint.cpp
+++ protomol/protomol/output/OutputCheckpoint.cpp
@@ -19,65 +19,65 @@ using namespace ProtoMol;
 const string OutputCheckpoint::keyword( "Checkpoint" );
 
 OutputCheckpoint::OutputCheckpoint() : mName( "false" ), mCurrent( 0 ),
-        mPosBase( "" ), mVelBase( "" ) {
-    if ( mName == "true" || mName == "True" || mName == "TRUE" ) {
-        isActive = true;
-    } else {
-        isActive = false;
-    }
+    mPosBase( "" ), mVelBase( "" ) {
+  if ( mName == "true" || mName == "True" || mName == "TRUE" ) {
+    isActive = true;
+  } else {
+    isActive = false;
+  }
 }
 
 OutputCheckpoint::OutputCheckpoint( const std::string& name, int freq,
                                     int start, const std::string& posbase,
                                     const std::string& velbase ) :
-        Output( freq ), mName( name ), mCurrent( start ),
-        mPosBase( posbase ), mVelBase( velbase ) {
+    Output( freq ), mName( name ), mCurrent( start ),
+    mPosBase( posbase ), mVelBase( velbase ) {
 
-    if ( mName == "true" || mName == "True" || mName == "TRUE" ) {
-        isActive = true;
-    } else {
-        isActive = false;
-    }
+  if ( mName == "true" || mName == "True" || mName == "TRUE" ) {
+    isActive = true;
+  } else {
+    isActive = false;
+  }
 }
 
 void OutputCheckpoint::doInitialize() {
-    if ( isActive ) {
-        if ( mPosBase == "" ) {
-            const std::string temp = app->config["posfile"];
+  if ( isActive ) {
+    if ( mPosBase == "" ) {
+      const std::string temp = app->config["posfile"];
 
-            mPosBase = temp.substr( 0, temp.rfind( '.' ) + 1 );
-        }
-
-        if ( mVelBase == "" ) {
-            if ( !app->config.valid( "velfile" ) ){
-                mVelBase = mPosBase;
-            }else{
-                const std::string temp = app->config["velfile"];
-
-                mVelBase = temp.substr( 0, temp.rfind( '.' ) + 1 );
-            }
-        }
+      mPosBase = temp.substr( 0, temp.rfind( '.' ) + 1 );
     }
+
+    if ( mVelBase == "" ) {
+      if ( !app->config.valid( "velfile" ) ) {
+        mVelBase = mPosBase;
+      } else {
+        const std::string temp = app->config["velfile"];
+
+        mVelBase = temp.substr( 0, temp.rfind( '.' ) + 1 );
+      }
+    }
+  }
 }
 
 void OutputCheckpoint::doRun( int step ) {
-    if ( isActive ) {
-        const int firstStep = toInt( app->config["firststep"] );
-        const int finalStep = firstStep + toInt( app->config["numsteps"] );
+  if ( isActive ) {
+    const int firstStep = toInt( app->config["firststep"] );
+    const int finalStep = firstStep + toInt( app->config["numsteps"] );
 
-        if ( step != firstStep && step != finalStep){
-            if ( ( step % myOutputFreq ) == 0 ){
-                ReadConfig();
-                WritePositions( step );
-                WriteVelocities( step );
-                WriteConfig( step );
+    if ( step != firstStep && step != finalStep ) {
+      if ( ( step % myOutputFreq ) == 0 ) {
+        ReadConfig();
+        WritePositions( step );
+        WriteVelocities( step );
+        WriteConfig( step );
 
-                mCurrent += 1;
+        mCurrent += 1;
 
-                std::cout << "Checkpointing: Step " << step << std::endl;
-            }
-        }
+        std::cout << "Checkpointing: Step " << step << std::endl;
+      }
     }
+  }
 }
 
 void OutputCheckpoint::doFinalize( int step ) {
@@ -85,128 +85,128 @@ void OutputCheckpoint::doFinalize( int step ) {
 }
 
 Output *OutputCheckpoint::doMake( const vector<Value> &values ) const {
-    return new OutputCheckpoint( values[0], values[1], values[2], values[3],
-                                 values[4] );
+  return new OutputCheckpoint( values[0], values[1], values[2], values[3],
+                               values[4] );
 }
 
 bool OutputCheckpoint::isIdDefined( const Configuration *config ) const {
-    return config->valid( getId() );
+  return config->valid( getId() );
 }
 
 void OutputCheckpoint::getParameters( vector<Parameter> &parameter ) const {
-    parameter.push_back(
-        Parameter( getId(),
-                   Value( mName, ConstraintValueType::NotEmpty() ) ) );
+  parameter.push_back(
+    Parameter( getId(),
+               Value( mName, ConstraintValueType::NotEmpty() ) ) );
 
-    parameter.push_back(
-        Parameter( keyword + "Freq",
-                   Value( myOutputFreq, ConstraintValueType::Positive() ) ) );
+  parameter.push_back(
+    Parameter( keyword + "Freq",
+               Value( myOutputFreq, ConstraintValueType::Positive() ) ) );
 
-    parameter.push_back(
-        Parameter( keyword + "Start",
-                   Value( mCurrent, ConstraintValueType::NotNegative() ) ) );
+  parameter.push_back(
+    Parameter( keyword + "Start",
+               Value( mCurrent, ConstraintValueType::NotNegative() ) ) );
 
-    parameter.push_back(
-        Parameter( keyword + "PosBase",
-                   Value( mPosBase, ConstraintValueType::NoConstraints() ) ) );
+  parameter.push_back(
+    Parameter( keyword + "PosBase",
+               Value( mPosBase, ConstraintValueType::NoConstraints() ) ) );
 
-    parameter.push_back(
-        Parameter( keyword + "VelBase",
-                   Value( mVelBase, ConstraintValueType::NoConstraints() ) ) );
+  parameter.push_back(
+    Parameter( keyword + "VelBase",
+               Value( mVelBase, ConstraintValueType::NoConstraints() ) ) );
 }
 
 bool OutputCheckpoint::adjustWithDefaultParameters( vector<Value> &values,
-        const Configuration *config ) const {
-    if ( !checkParameterTypes( values ) ) return false;
+    const Configuration *config ) const {
+  if ( !checkParameterTypes( values ) ) return false;
 
-    if ( !values[0].valid() ) {
-        values[0] = mName;
-    }
+  if ( !values[0].valid() ) {
+    values[0] = mName;
+  }
 
-    if ( !values[1].valid() ) {
-        values[1] = 1;
-    }
+  if ( config->valid( InputMinimalImage::keyword ) && !values[1].valid() ) {
+    values[1] = ( *config )[InputMinimalImage::keyword];
+  }
 
-    if ( !values[2].valid() ) {
-        values[2] = 0;
-    }
+  if ( !values[2].valid() ) {
+    values[2] = 0;
+  }
 
-    if ( !values[3].valid() ) {
-        values[3] = "";
-    }
+  if ( !values[3].valid() ) {
+    values[3] = "";
+  }
 
-    if ( !values[4].valid() ) {
-        values[4] = "";
-    }
+  if ( !values[4].valid() ) {
+    values[4] = "";
+  }
 
 
-    return checkParameters( values );
+  return checkParameters( values );
 }
 
 void OutputCheckpoint::ReadConfig( ) {
-    ifstream inFile( Append( mPosBase, "dat").c_str() );
+  ifstream inFile( Append( mPosBase, "dat" ).c_str() );
 
-    if ( inFile ) {
-      ofstream outFile( Append( mPosBase, "last").c_str() );
+  if ( inFile ) {
+    ofstream outFile( Append( mPosBase, "last" ).c_str() );
 
-      if ( outFile ){
-          std::string line;
+    if ( outFile ) {
+      std::string line;
 
-          while ( std::getline( inFile, line ) ) {
-              outFile << line << '\n';
-          }
+      while ( std::getline( inFile, line ) ) {
+        outFile << line << '\n';
       }
     }
+  }
 }
 
 void OutputCheckpoint::WritePositions( int step ) {
-    std::string posFile = Append( Append( mPosBase, mCurrent ), ".pos" );
+  std::string posFile = Append( Append( mPosBase, mCurrent ), ".pos" );
 
-    XYZWriter posWriter;
-    if ( !posWriter.open( posFile ) ) {
-        THROW( string( "Can't open " ) + getId() + " '" + posFile + "'." );
-    }
+  XYZWriter posWriter;
+  if ( !posWriter.open( posFile ) ) {
+    THROW( string( "Can't open " ) + getId() + " '" + posFile + "'." );
+  }
 
-    const Vector3DBlock *pos = &app->positions;
-    posWriter.setComment( "Time : " + toString( app->outputCache.time() ) +
-                          ", step : " + toString( step ) +  "." );
+  const Vector3DBlock *pos = &app->positions;
+  posWriter.setComment( "Time : " + toString( app->outputCache.time() ) +
+                        ", step : " + toString( step ) +  "." );
 
-    if ( !posWriter.write( *pos, app->topology->atoms, app->topology->atomTypes ) ) {
-        THROW( string( "Could not write " ) + getId() + " '" + posFile + "'." );
-    }
+  if ( !posWriter.write( *pos, app->topology->atoms, app->topology->atomTypes ) ) {
+    THROW( string( "Could not write " ) + getId() + " '" + posFile + "'." );
+  }
 }
 
 void OutputCheckpoint::WriteVelocities( int step ) {
-    std::string velFile = Append( Append( mVelBase, mCurrent ), ".vel" );
+  std::string velFile = Append( Append( mVelBase, mCurrent ), ".vel" );
 
-    XYZWriter velWriter;
-    if ( !velWriter.open( velFile ) ) {
-        THROW( string( "Can't open " ) + getId() + " '" + velFile + "'." );
-    }
+  XYZWriter velWriter;
+  if ( !velWriter.open( velFile ) ) {
+    THROW( string( "Can't open " ) + getId() + " '" + velFile + "'." );
+  }
 
-    velWriter.setComment( "Time : " + toString( app->outputCache.time() ) +
-                          ", step : " + toString( step ) + "." );
+  velWriter.setComment( "Time : " + toString( app->outputCache.time() ) +
+                        ", step : " + toString( step ) + "." );
 
-    if ( !velWriter.write( *&app->velocities, app->topology->atoms,
-                           app->topology->atomTypes ) ) {
-        THROW( string( "Could not write " ) + getId() + " '" + velFile + "'." );
-    }
+  if ( !velWriter.write( *&app->velocities, app->topology->atoms,
+                         app->topology->atomTypes ) ) {
+    THROW( string( "Could not write " ) + getId() + " '" + velFile + "'." );
+  }
 }
 
 void OutputCheckpoint::WriteConfig( int step ) {
-    ofstream outFile( Append( mPosBase, "dat").c_str() );
+  ofstream outFile( Append( mPosBase, "dat" ).c_str() );
 
-    if ( outFile ) {
-        outFile << "#ID" << std::endl;
-        outFile << mCurrent << std::endl;
+  if ( outFile ) {
+    outFile << "#ID" << std::endl;
+    outFile << mCurrent << std::endl;
 
-        outFile << "#Step" << std::endl;
-        outFile << step << std::endl;
+    outFile << "#Step" << std::endl;
+    outFile << step << std::endl;
 
-        outFile << "#Random" << std::endl;
-        outFile << Random::Instance() << std::endl;
+    outFile << "#Random" << std::endl;
+    outFile << Random::Instance() << std::endl;
 
-        outFile << "#Integrator" << std::endl;
-        outFile << (*app->integrator) << std::endl;
-    }
+    outFile << "#Integrator" << std::endl;
+    outFile << ( *app->integrator ) << std::endl;
+  }
 }
