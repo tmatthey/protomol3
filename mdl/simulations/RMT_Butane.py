d30a58de93def5f724d2213075b39269fcab7e28
tcickovs <tcickovs@ce46b6ef-3445-0410-911d-c47028e6f9df>
2009-05-05 13:23:13 +0000

    Removed output statements
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1544 ce46b6ef-3445-0410-911d-c47028e6f9df

--- mdl/simulations/RMT_Butane.py
+++ mdl/simulations/RMT_Butane.py
@@ -4,7 +4,6 @@ from MDL import *
 # PHYSICAL SYSTEM
 phys = Physical()
 io = IO()
-#io.doMPL = 1
 io.readPDBPos(phys, "data/UA_butane/UA_butane.pdb")
 io.readPSF(phys, "data/UA_butane/UA_butane.psf")
 io.readPAR(phys, "data/UA_butane/UA_butane.par")
@@ -14,22 +13,14 @@ phys.temperature = 300
 
 # FORCES
 forces = Forces()
-print "FF"
-#ff = forces.makeForceField(phys)
 ff = forces.makeForceField(phys, "charmm")
-print "GG"
 
 # OUTPUT
-#io.plots = {'kineticenergy':4}
-print "HH"
 io.screen = 2
 
-print "II"
 # PROPAGATION
-print "SS"
 prop = Propagator(phys, forces, io)
-print "PROPAGATE"
 prop.propagate(scheme="RMT", steps=200, dt=0.5, forcefield=ff)
 
 
-    
+
