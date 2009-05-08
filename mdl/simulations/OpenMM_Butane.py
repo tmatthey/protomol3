76f5f22c93806d0c964fdbf83ac412b923437159
tcickovs <tcickovs@ce46b6ef-3445-0410-911d-c47028e6f9df>
2009-05-08 14:11:12 +0000

    Added a harmonic bond force, just so it's clear how to use this integrator
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1560 ce46b6ef-3445-0410-911d-c47028e6f9df

--- mdl/simulations/OpenMM_Butane.py
+++ mdl/simulations/OpenMM_Butane.py
@@ -4,7 +4,6 @@ from MDL import *
 # PHYSICAL SYSTEM
 phys = Physical()
 io = IO()
-#io.doMPL = 1
 io.readPDBPos(phys, "data/UA_butane/UA_butane.pdb")
 io.readPSF(phys, "data/UA_butane/UA_butane.psf")
 io.readPAR(phys, "data/UA_butane/UA_butane.par")
@@ -21,7 +20,8 @@ io.screen = 2
 
 # PROPAGATION
 prop = Propagator(phys, forces, io)
-prop.propagate(scheme="OpenMM", steps=200, dt=0.5, forcefield=ff)
+prop.propagate(scheme="OpenMM", steps=200, dt=0.5, forcefield=ff,
+               params={'HarmonicBondForce':True})
 
 
 
