425a9eee122d740e5285b27add9a8baa6fd18103
jcofflan <jcofflan@ce46b6ef-3445-0410-911d-c47028e6f9df>
2009-09-21 11:29:10 +0000

    Need to initialize your variables\!
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1693 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/protomol/topology/GenericTopology.cpp
+++ protomol/protomol/topology/GenericTopology.cpp
@@ -15,14 +15,16 @@ GenericTopology::GenericTopology() :
   min(Vector3D(Constant::MAXREAL, Constant::MAXREAL, Constant::MAXREAL)),
   max(Vector3D(-Constant::MINREAL, -Constant::MINREAL, -Constant ::MINREAL)),
   implicitSolvent(NONE), doSCPISM(0), forceFieldFlag(CHARMM), 
-  minimalMolecularDistances(false) {}
+  minimalMolecularDistances(false), doGBSAOpenMM(0), obcType(0),
+  dielecOffset(0), alphaObc(0), betaObc(0), gammaObc(0) {}
 
 GenericTopology::GenericTopology(Real c, const ExclusionType &e) :
   exclude(e), coulombScalingFactor(c), time(0.0),
   min(Vector3D(Constant::MAXREAL, Constant::MAXREAL, Constant::MAXREAL)),
   max(Vector3D(-Constant::MINREAL, -Constant::MINREAL, -Constant::MINREAL)),
   implicitSolvent(NONE), doSCPISM(0), forceFieldFlag(CHARMM), 
-  minimalMolecularDistances(false) {}
+  minimalMolecularDistances(false), doGBSAOpenMM(0), obcType(0),
+  dielecOffset(0), alphaObc(0), betaObc(0), gammaObc(0) {}
 
 GenericTopology *GenericTopology::make(const vector<Value> &values) const {
   assertParameters(values);
