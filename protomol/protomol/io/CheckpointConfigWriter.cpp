143ed6e084053398082beacec98c743c2e48eb70
jcofflan <jcofflan@ce46b6ef-3445-0410-911d-c47028e6f9df>
2009-09-21 12:04:46 +0000

    Opening the CheckpointConfigWriter file twice cause problems in Windows
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1695 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/protomol/io/CheckpointConfigWriter.cpp
+++ protomol/protomol/io/CheckpointConfigWriter.cpp
@@ -1,40 +1,25 @@
 #include <protomol/io/CheckpointConfigWriter.h>
 
-#include <iomanip>
-
-#include <protomol/base/Report.h>
-#include <protomol/base/StringUtilities.h>
-
-using namespace ProtoMol::Report;
 using namespace ProtoMol;
 
-//____CheckpointConfigWriter
+CheckpointConfigWriter::CheckpointConfigWriter() : Writer() {}
 
-CheckpointConfigWriter::CheckpointConfigWriter() :
-  Writer() {}
-
-CheckpointConfigWriter::CheckpointConfigWriter( const std::string& filename ) :
+CheckpointConfigWriter::CheckpointConfigWriter(const std::string &filename) :
   Writer(filename) {}
 
-bool CheckpointConfigWriter::write( const int& id, const int& steps, const Random& rand, const Integrator* integ ) {
-    if (!open() ){
-        return false;
-    }
+bool CheckpointConfigWriter::write(const int &id, const int &steps,
+                                   const Random &rand,
+                                   const Integrator *integ) {
+  file
+    << "!Checkpoint File!" << std::endl
+    << "#ID" << std::endl
+    << id << std::endl
+    << "#Step" << std::endl
+    << steps << std::endl
+    << "#Random" << std::endl
+    << rand << std::endl
+    << "#Integrator" << std::endl
+    << *integ << std::endl;
 
-    file << "!Checkpoint File!" << std::endl;
-
-    file << "#ID" << std::endl;
-    file << id << std::endl;
-
-    file << "#Step" << std::endl;
-    file << steps << std::endl;
-
-    file << "#Random" << std::endl;
-    file << rand << std::endl;
-
-    file << "#Integrator" << std::endl;
-    file << (*integ) << std::endl;
-
-    close();
-    return !file.fail();
+  return !file.fail();
 }
