0561933b25fb73e2f47f678780dd23a1cd6801ad
jcofflan <jcofflan@ce46b6ef-3445-0410-911d-c47028e6f9df>
2009-12-01 22:46:30 +0000

    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1735 ce46b6ef-3445-0410-911d-c47028e6f9df

--- fah-core/protomol.cpp
+++ fah-core/protomol.cpp
@@ -22,7 +22,7 @@ extern void moduleInitFunction(ModuleManager *);
 
 class ProtoMolCore : public Core {
 public:
-  ProtoMolCore() : Core("ProtoMol", 180, 15) {}
+  ProtoMolCore() : Core("ProtoMol", 180, 16) {}
 
   int init(int argc, char *argv[]) {
     getOptions().add("steps-per-gen")->setType(Option::INTEGER_TYPE);
