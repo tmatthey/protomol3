2bed34d5761ff630af00bf1593665b99f04344f9
chrissweet <chrissweet@ce46b6ef-3445-0410-911d-c47028e6f9df>
2010-03-12 16:10:45 +0000

    New LJ force with "residue"=cell exclusion.
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1773 ce46b6ef-3445-0410-911d-c47028e6f9df

--- applications/protocell/module/VesselForcesModule.cpp
+++ applications/protocell/module/VesselForcesModule.cpp
@@ -1,14 +1,22 @@
 #include "VesselForcesModule.h"
 
-#include <protomol/force/bonded/RBDihedralSystemForce.h>
 #include <protomol/force/bonded/DihedralSystemForce.h>
 #include "../force/VesselForce.h"
+#include "../force/LJSCEExcludeForce.h"
 
 #include <protomol/ProtoMolApp.h>
 #include <protomol/base/StringUtilities.h>
 #include <protomol/module/TopologyModule.h>
 #include <protomol/topology/PeriodicBoundaryConditions.h>
 #include <protomol/topology/VacuumBoundaryConditions.h>
+
+//
+#include <protomol/switch/C2SwitchingFunction.h>
+#include <protomol/switch/CnSwitchingFunction.h>
+#include <protomol/force/OneAtomPair.h>
+#include <protomol/topology/CellListEnumeratorStandard.h>
+#include <protomol/force/nonbonded/NonbondedCutoffSystemForce.h>
+#include <protomol/topology/CellListEnumeratorPeriodicBoundaries.h>
 
 using namespace std;
 using namespace ProtoMol;
@@ -21,11 +29,35 @@ void VesselForcesModule::registerForces(ProtoMolApp *app) {
   string boundConds =
     app->config[InputBoundaryConditions::keyword];
 
+  //Periodic BC?
   if (equalNocase(boundConds, PeriodicBoundaryConditions::keyword)) {
+    //Vessel wall
     f.registerExemplar(new VesselForce<PeriodicBoundaryConditions>());
 
+    //Inter-SCE LJ, C2 and Cn switching
+    f.reg(new NonbondedCutoffSystemForce<CubicCellManager,
+            OneAtomPair<PeriodicBoundaryConditions,
+                C2SwitchingFunction, LJSCEExcludeForce> >());
+    f.reg(new NonbondedCutoffSystemForce<CubicCellManager,
+            OneAtomPair<PeriodicBoundaryConditions,
+                CnSwitchingFunction, LJSCEExcludeForce> >());
+
+
+  //Vacuum?
   } else if (equalNocase(boundConds, VacuumBoundaryConditions::keyword)) {
+
+    //Vessel wall
     f.registerExemplar(new VesselForce<VacuumBoundaryConditions>());
 
+    //Inter-SCE LJ, C2 and Cn switching
+    f.reg(new NonbondedCutoffSystemForce<CubicCellManager,
+            OneAtomPair<VacuumBoundaryConditions,
+                C2SwitchingFunction, LJSCEExcludeForce> >());
+    f.reg(new NonbondedCutoffSystemForce<CubicCellManager,
+            OneAtomPair<VacuumBoundaryConditions,
+                CnSwitchingFunction, LJSCEExcludeForce> >());
+
+
   }
+  
 }
