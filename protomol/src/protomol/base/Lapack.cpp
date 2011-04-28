77440ea7f652389dd785eabbab958819e1ea7735
omegaice <omegaice@ce46b6ef-3445-0410-911d-c47028e6f9df>
2011-04-28 17:01:31 +0000

    Updated lapack calls to checkin with folding@home if it is being used
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1886 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/src/protomol/base/Lapack.cpp
+++ protomol/src/protomol/base/Lapack.cpp
@@ -1,15 +1,27 @@
 #include "protomol/base/Lapack.h"
 
 #if defined(HAVE_LAPACK)
-	#include <protomol/integrator/hessian/LapackProtomol.h>
+#include <protomol/integrator/hessian/LapackProtomol.h>
 #elif defined(HAVE_SIMTK_LAPACK)
-	#include <SimTKlapack.h>
+#include <SimTKlapack.h>
 #elif defined(HAVE_MKL_LAPACK)
-	#include <mkl_blas.h>
-	#include <mkl_lapack.h>
+#include <mkl_blas.h>
+#include <mkl_lapack.h>
 #endif
 
-bool Lapack::isEnabled(){
+#ifdef HAVE_LIBFAH
+#include <fah/core/Core.h>
+#endif
+
+void FAHCheckIn() {
+#ifdef HAVE_LIBFAH
+	if( FAH::Core::isActive() ) {
+		FAH::Core::instance().checkIn();
+	}
+#endif
+}
+
+bool Lapack::isEnabled() {
 #if defined(HAVE_LAPACK)
 	return true;
 #elif defined(HAVE_SIMTK_LAPACK)
@@ -20,48 +32,53 @@ bool Lapack::isEnabled(){
 	return false;
 }
 
-void Lapack::dgemv(char *transA, int *m, int *n, double *alpha, double *A, int *lda, double *x, int *incx, double *beta, double *Y, int *incY){
+void Lapack::dgemv( char *transA, int *m, int *n, double *alpha, double *A, int *lda, double *x, int *incx, double *beta, double *Y, int *incY ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	dgemv_(transA, m, n, alpha, A, lda, x, incx, beta, Y, incY );
+	dgemv_( transA, m, n, alpha, A, lda, x, incx, beta, Y, incY );
 #elif defined(HAVE_SIMTK_LAPACK)
-	dgemv_(*transA, *m, *n, *alpha, A, *lda, x, *incx, *beta, Y, *incY, 1);
+	dgemv_( *transA, *m, *n, *alpha, A, *lda, x, *incx, *beta, Y, *incY, 1 );
 #elif defined(HAVE_MKL_LAPACK)
-	DGEMV(transA, m, n, alpha, A, lda, x, incx, beta, Y, incY );
+	DGEMV( transA, m, n, alpha, A, lda, x, incx, beta, Y, incY );
 #endif
 }
 
-void Lapack::dsyev(char *jobz, char *uplo, int *n, double *a, int *lda, double *w, double *work, int *lwork, int *info){
+void Lapack::dsyev( char *jobz, char *uplo, int *n, double *a, int *lda, double *w, double *work, int *lwork, int *info ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	dsyev_(jobz, uplo, n, a, lda, w, work, lwork, info);
+	dsyev_( jobz, uplo, n, a, lda, w, work, lwork, info );
 #elif defined(HAVE_SIMTK_LAPACK)
-	dsyev_(*jobz, *uplo, *n, a, *lda, w, work, *lwork, *info);
+	dsyev_( *jobz, *uplo, *n, a, *lda, w, work, *lwork, *info );
 #elif defined(HAVE_MKL_LAPACK)
-	DSYEV(jobz, uplo, n, a, lda, w, work, lwork, info);
+	DSYEV( jobz, uplo, n, a, lda, w, work, lwork, info );
 #endif
 }
 
-void Lapack::dsyevr(char *jobz, char *range, char *uplo, int *n, double *a, int *lda, double *vl, double *vu, int *il, int *iu, double *abstol, int *m, double *w, double *z,  int *ldz, int *isuppz, double *work, int *lwork, int *iwork, int *liwork, int *info){
+void Lapack::dsyevr( char *jobz, char *range, char *uplo, int *n, double *a, int *lda, double *vl, double *vu, int *il, int *iu, double *abstol, int *m, double *w, double *z,  int *ldz, int *isuppz, double *work, int *lwork, int *iwork, int *liwork, int *info ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	dsyevr_(jobz, range, uplo, n, a, lda, vl, vu, il, iu, abstol, m, w, z, ldz, isuppz, work, lwork, iwork, liwork, info);
+	dsyevr_( jobz, range, uplo, n, a, lda, vl, vu, il, iu, abstol, m, w, z, ldz, isuppz, work, lwork, iwork, liwork, info );
 #elif defined(HAVE_SIMTK_LAPACK)
-	dsyevr_( *jobz, *range, *uplo, *n, a, *lda, vl, vu, il, iu, abstol, *m, w, z, *ldz, isuppz, work, *lwork, iwork, liwork, *info, 1, 1, 1);
+	dsyevr_( *jobz, *range, *uplo, *n, a, *lda, vl, vu, il, iu, abstol, *m, w, z, *ldz, isuppz, work, *lwork, iwork, liwork, *info, 1, 1, 1 );
 #elif defined(HAVE_MKL_LAPACK)
-	DSYEVR(jobz, range, uplo, n, a, lda, vl, vu, il, iu, abstol, m, w, z, ldz, isuppz, work, lwork, iwork, liwork, info);
+	DSYEVR( jobz, range, uplo, n, a, lda, vl, vu, il, iu, abstol, m, w, z, ldz, isuppz, work, lwork, iwork, liwork, info );
 #endif
 }
 
-double Lapack::dlamch(char *cmach){
+double Lapack::dlamch( char *cmach ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	return dlamch_(cmach);
+	return dlamch_( cmach );
 #elif defined(HAVE_SIMTK_LAPACK)
 	return dlamch_( *cmach, 1 );
 #elif defined(HAVE_MKL_LAPACK)
-	return DLAMCH(cmach);
+	return DLAMCH( cmach );
 #endif
 	return 0.0;
 }
 
-void Lapack::dgemm(char *transA, char *transB, int *m, int *n, int *k, double *alpha, double *A, int *lda, double *B, int *ldb, double *beta, double *C, int *l){
+void Lapack::dgemm( char *transA, char *transB, int *m, int *n, int *k, double *alpha, double *A, int *lda, double *B, int *ldb, double *beta, double *C, int *l ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
 	dgemm_( transA, transB, m, n, k, alpha, A, lda, B, ldb, beta, C, l );
 #elif defined(HAVE_SIMTK_LAPACK)
@@ -71,7 +88,8 @@ void Lapack::dgemm(char *transA, char *transB, int *m, int *n, int *k, double *a
 #endif
 }
 
-double Lapack::ddot(int *n, double *x, int *incx, double *y, int *incy){
+double Lapack::ddot( int *n, double *x, int *incx, double *y, int *incy ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
 	return ddot_( n, x, incx, y, incy );
 #elif defined(HAVE_SIMTK_LAPACK)
@@ -82,7 +100,8 @@ double Lapack::ddot(int *n, double *x, int *incx, double *y, int *incy){
 	return 0.0;
 }
 
-double Lapack::dnrm2(int *n, double *x, int *incx){
+double Lapack::dnrm2( int *n, double *x, int *incx ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
 	return dnrm2_( n, x, incx );
 #elif defined(HAVE_SIMTK_LAPACK)
@@ -93,47 +112,52 @@ double Lapack::dnrm2(int *n, double *x, int *incx){
 	return 0.0;
 }
 
-void Lapack::dpotri(char *transA, int *n, double *A, int *lda, int *info){
+void Lapack::dpotri( char *transA, int *n, double *A, int *lda, int *info ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	dpotri_(transA, n, A, lda, info);
+	dpotri_( transA, n, A, lda, info );
 #elif defined(HAVE_SIMTK_LAPACK)
 #elif defined(HAVE_MKL_LAPACK)
-	DPOTRI(transA, n, A, lda, info);
+	DPOTRI( transA, n, A, lda, info );
 #endif
 }
 
-void Lapack::dpotrf(char *transA, int *n, double *A, int *lda, int *info){
+void Lapack::dpotrf( char *transA, int *n, double *A, int *lda, int *info ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	dpotrf_(transA, n, A, lda, info);
+	dpotrf_( transA, n, A, lda, info );
 #elif defined(HAVE_SIMTK_LAPACK)
 #elif defined(HAVE_MKL_LAPACK)
-	DPOTRF(transA, n, A, lda, info);
+	DPOTRF( transA, n, A, lda, info );
 #endif
 }
 
-void Lapack::dposv(char *transA, int *n, int *nrhs, double *a, int *lda, double *b, int *ldb, int *info){
+void Lapack::dposv( char *transA, int *n, int *nrhs, double *a, int *lda, double *b, int *ldb, int *info ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	dposv_(transA, n, nrhs, a, lda, b, ldb, info);
+	dposv_( transA, n, nrhs, a, lda, b, ldb, info );
 #elif defined(HAVE_SIMTK_LAPACK)
 #elif defined(HAVE_MKL_LAPACK)
-	DPOSV(transA, n, nrhs, a, lda, b, ldb, info);
+	DPOSV( transA, n, nrhs, a, lda, b, ldb, info );
 #endif
 }
 
-void Lapack::dtrmm(char *sideA, char *ulA, char *transA, char *diagA, int *m, int *n, double *alpha, double *A, int *lda, double *B, int *ldb){
+void Lapack::dtrmm( char *sideA, char *ulA, char *transA, char *diagA, int *m, int *n, double *alpha, double *A, int *lda, double *B, int *ldb ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	dtrmm_(sideA, ulA, transA, diagA, m, n, alpha, A, lda, B, ldb);
+	dtrmm_( sideA, ulA, transA, diagA, m, n, alpha, A, lda, B, ldb );
 #elif defined(HAVE_SIMTK_LAPACK)
 #elif defined(HAVE_MKL_LAPACK)
-	DTRMM(sideA, ulA, transA, diagA, m, n, alpha, A, lda, B, ldb);
+	DTRMM( sideA, ulA, transA, diagA, m, n, alpha, A, lda, B, ldb );
 #endif
 }
 
-void Lapack::dtrsm(char *sideA, char *ulA, char *transA, char *diagA, int *m, int *n, double *alpha, double *A, int *lda, double *B, int *ldb){
+void Lapack::dtrsm( char *sideA, char *ulA, char *transA, char *diagA, int *m, int *n, double *alpha, double *A, int *lda, double *B, int *ldb ) {
+	FAHCheckIn();
 #if defined(HAVE_LAPACK)
-	dtrsm_(sideA, ulA, transA, diagA, m, n, alpha, A, lda, B, ldb);
+	dtrsm_( sideA, ulA, transA, diagA, m, n, alpha, A, lda, B, ldb );
 #elif defined(HAVE_SIMTK_LAPACK)
 #elif defined(HAVE_MKL_LAPACK)
-	DTRSM(sideA, ulA, transA, diagA, m, n, alpha, A, lda, B, ldb);
+	DTRSM( sideA, ulA, transA, diagA, m, n, alpha, A, lda, B, ldb );
 #endif
 }
