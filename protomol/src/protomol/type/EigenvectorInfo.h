514ce79495f4a3bb71adf5d268e9f36555be405b
omegaice <omegaice@ce46b6ef-3445-0410-911d-c47028e6f9df>
2012-02-03 14:19:52 +0000

    Moved code from EigenvectorInfo to its own file
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@1983 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/src/protomol/type/EigenvectorInfo.h
+++ protomol/src/protomol/type/EigenvectorInfo.h
@@ -2,125 +2,57 @@
 #ifndef EIGENVECTORINFO_H
 #define EIGENVECTORINFO_H
 
-#include <string>
-#include <iostream>
 #include <vector>
 
-using std::cout;
-using std::endl;
-namespace ProtoMol
-{
-  //_________________________________________________________________XYZ
-  /**
-   * Container holding coordinates/Vector3D and names
-   */
-  struct EigenvectorInfo {
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Constructors, destructors, assignment
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    EigenvectorInfo() : myEigenvectorLength ( 0 ), myNumEigenvectors ( 0 ), 
-        myNumUsedEigenvectors( 0 ),
-        myEigenvectors ( 0 ), myOrigCEigval( 0.0 ), myNewCEigval( 0.0 ), myOrigTimestep( 0.0 ), 
-        reDiagonalize( false ),
-        mySingleEigs( 0 ), myEigVecChanged( true ), myMinimumLimit( 0.5 ), currentMode( -1 ) {};
+namespace ProtoMol {
+	/**
+	 * Container holding coordinates/Vector3D and names
+	 */
+	struct EigenvectorInfo {
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		// Constructors, destructors, assignment
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		EigenvectorInfo();
+		EigenvectorInfo( unsigned int n, unsigned int m );
 
-    EigenvectorInfo( unsigned int n, unsigned int m ) : myEigenvectorLength( n ), 
-        myNumEigenvectors( m ), myNumUsedEigenvectors( 0 ),
-        myMaxEigenvalue( 0.0 ), myEigenvectors ( new double[n * m * 3] ), 
-        myOrigCEigval( 0.0 ), myNewCEigval( 0.0 ), myOrigTimestep( 0.0 ), reDiagonalize( false ),
-        mySingleEigs( 0 ),
-        myEigVecChanged( true ), myMinimumLimit( 0.5 ), currentMode( -1 ) {}
+		~EigenvectorInfo();
 
-    ~EigenvectorInfo() {
-      if ( myEigenvectors ) {
-        delete [] myEigenvectors;
-        myEigenvectors = 0;
-      }
-      if ( mySingleEigs ) {
-        delete [] mySingleEigs;
-        mySingleEigs = 0;
-      }
-    };
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		// New methods of class EigenvectorInfo
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		bool initializeEigenvectors();
+		float *getFloatEigPointer();
 
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // New methods of class EigenvectorInfo
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    bool initializeEigenvectors() {
-      try {
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		// My data members
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        myEigenvectors = new double[myEigenvectorLength * myNumEigenvectors * 3];
+		// Eigenvector information
+		unsigned int myEigenvectorLength;
+		unsigned int myNumEigenvectors;
+		unsigned int myNumUsedEigenvectors;
+		double myMaxEigenvalue;
+		double *myEigenvectors;
 
-      } catch ( std::bad_alloc& ) {
+		//Current and original max subspace eigenvalue,
+		//for adaptive timestep
+		double myOrigCEigval, myNewCEigval;
+		double myOrigTimestep;
 
-        return false;
+		//re-diagonalization flag
+		bool reDiagonalize;
+		bool havePositionsChanged;
 
-      }
+		//OpenMM single precision interface
+		float *mySingleEigs;
+		bool myEigVecChanged;
 
-      return true;
+		double myMinimumLimit;
 
-    }
-
-    float* getFloatEigPointer() {
-
-      //no eigenvectors assigned?
-      if(!myEigenvectors) return (float*)0;
-
-      const unsigned int arrayLen = myEigenvectorLength * myNumEigenvectors * 3;
-
-      //assign storage if required
-      if(!mySingleEigs) {  
-
-        try {
-          mySingleEigs = new float[arrayLen];
-        } catch ( std::bad_alloc& ) {
-          return (float*)0;
-        }
-
-      }
-
-      //update values if double array updated
-      if(myEigVecChanged) {
-
-        for( unsigned int i=0; i<arrayLen; i++)
-          mySingleEigs[i] = (float)myEigenvectors[i];
-
-        myEigVecChanged = false;
-
-      }
-
-      return mySingleEigs;
-
-    }
-
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // My data members
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Eigenvector information
-    unsigned int myEigenvectorLength;
-    unsigned int myNumEigenvectors;
-    unsigned int myNumUsedEigenvectors;
-    double myMaxEigenvalue;
-    double *myEigenvectors;
-    
-    //Current and original max subspace eigenvalue, 
-    //for adaptive timestep
-    double myOrigCEigval, myNewCEigval;
-    double myOrigTimestep;
-
-    //re-diagonalization flag
-    bool reDiagonalize;
-    
-    //OpenMM single precision interface
-    float *mySingleEigs;
-    bool myEigVecChanged;
-
-    double myMinimumLimit;
-
-    //Analytic integrator
-    std::vector< double > myEigenvalues;
-    int currentMode;
-
-
-  };
+		//Analytic integrator
+		std::vector< double > myEigenvalues;
+		int currentMode;
+	};
 }
-#endif /* EIGENVECTORINFO_H */
+
+#endif // EIGENVECTORINFO_H
