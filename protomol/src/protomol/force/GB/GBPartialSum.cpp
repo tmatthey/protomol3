f70dcc38bd0c56fecc401813891dd43bd0272c4a
Omegaice <Omegaice@ce46b6ef-3445-0410-911d-c47028e6f9df>
2012-03-29 18:03:16 +0000

    Moved GBPartialSum from its header into a source file
    git-svn-id: http://svn.code.sf.net/p/protomol/svn/trunk@2069 ce46b6ef-3445-0410-911d-c47028e6f9df

--- protomol/src/protomol/force/GB/GBPartialSum.cpp
+++ protomol/src/protomol/force/GB/GBPartialSum.cpp
@@ -1,6 +1,87 @@
 #include <protomol/force/GB/GBPartialSum.h>
 
-using namespace std;
+#include <protomol/parallel/Parallel.h>
+
 using namespace ProtoMol;
 
 const string GBPartialSum::keyword("GBPartialSum");
+
+GBPartialSum::GBPartialSum(){}
+
+void GBPartialSum::operator()(Real &energy, Real &force,
+                Real distSquared, Real rDistSquared, const Vector3D &diff,
+                const GenericTopology *topo,
+                int atom1, int atom2, ExclusionClass excl) const {
+  
+  Real bornRad_i = topo->atoms[atom1].myGBSA_T->bornRad;
+  Real bornRad_l = topo->atoms[atom2].myGBSA_T->bornRad;
+  
+  Real scaledCharge_i = topo->atoms[atom1].scaledCharge;
+  Real scaledCharge_l = topo->atoms[atom2].scaledCharge;
+  
+  Real ril = sqrt(distSquared);
+  
+  Real expterm = (ril*ril)/(4.0*bornRad_i*bornRad_l);
+  Real filGB = sqrt(ril*ril + bornRad_i*bornRad_l*exp(-expterm));
+  
+  topo->atoms[atom1].myGBSA_T->partialGBForceTerms += scaledCharge_i*scaledCharge_l*(1/(filGB*filGB))*0.5*(1/filGB)*exp(-expterm)*(bornRad_l + (ril*ril)/(4.0*bornRad_i));
+  topo->atoms[atom2].myGBSA_T->partialGBForceTerms += scaledCharge_i*scaledCharge_l*(1/(filGB*filGB))*0.5*(1/filGB)*exp(-expterm)*(bornRad_i + (ril*ril)/(4.0*bornRad_l));
+}
+
+void GBPartialSum::accumulateEnergy(ScalarStructure *energies, Real energy) {
+  
+}
+
+Real GBPartialSum::getEnergy(const ScalarStructure *energies) {
+  return 0;
+}
+
+void GBPartialSum::postProcess(const GenericTopology *topo, ScalarStructure *energies) {
+  const unsigned int atomnumber = topo->atoms.size();
+  
+  for( unsigned int i=0; i<atomnumber; i++)
+    topo->atoms[i].myGBSA_T->havePartialGBForceTerms = true;
+}
+
+void GBPartialSum::parallelPostProcess(const GenericTopology *topo, ScalarStructure *energies) {
+  const unsigned int atomnumber = topo->atoms.size();
+  
+  // Copy Radii
+  Real *partial = new Real[ atomnumber ];
+  
+  //put radii into array
+  for( unsigned int i = 0; i < atomnumber; i++ ){
+    partial[i] = topo->atoms[i].myGBSA_T->partialGBForceTerms;
+  }
+  
+  //sum accross nodes
+  Parallel::reduce(partial, partial + atomnumber); //reduceSlaves only?
+  
+  //put radii back
+  for( unsigned int i = 0; i < atomnumber; i++ ){
+    topo->atoms[i].myGBSA_T->partialGBForceTerms = partial[i];
+  }
+  
+  delete [] partial;
+}
+
+bool GBPartialSum::doParallelPostProcess() {
+  return true;
+}
+
+// Parsing
+std::string GBPartialSum::getId() {
+  return keyword;
+}
+
+unsigned int GBPartialSum::getParameterSize() {
+  return 0;
+}
+
+void GBPartialSum::getParameters(std::vector<Parameter> &) const {
+
+}
+
+GBPartialSum GBPartialSum::make(const std::vector<Value> &) {
+  return GBPartialSum();
+}
--- protomol/src/protomol/force/GB/GBPartialSum.h
+++ protomol/src/protomol/force/GB/GBPartialSum.h
@@ -2,127 +2,39 @@
 #ifndef GBPARTIALSUM_H
 #define GBPARTIALSUM_H
 
+#include <string>
 
-#include <protomol/topology/GenericTopology.h>
 #include <protomol/type/ScalarStructure.h>
 #include <protomol/topology/ExclusionTable.h>
-#include <protomol/config/Parameter.h>
-#include <protomol/base/MathUtilities.h>
-#include <protomol/parallel/Parallel.h>
-
-#include <protomol/base/Report.h>
-
-using namespace ProtoMol::Report;
+#include <protomol/topology/GenericTopology.h>
 
 namespace ProtoMol {
-  
   class GBPartialSum {
-    
-  public:
-    
-    enum {DIST_R2 = 1};
-    enum {CUTOFF = 0};
-    
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Constructors, destructors, assignment
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
-    GBPartialSum(){}
-    
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // New methods of class GBBornRadii
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
-  public:
-    
-    void operator()(Real &energy, Real &force,
-                    Real distSquared, Real rDistSquared, const Vector3D &diff,
-                    const GenericTopology *topo,
-                    int atom1, int atom2, ExclusionClass excl) const {
-      
-      Real scaledCharge_i, scaledCharge_l;
-      
-      Real bornRad_i, bornRad_l;
-      
-      Real filGB, expterm;
-      
-      bornRad_i = topo->atoms[atom1].myGBSA_T->bornRad;
-      bornRad_l = topo->atoms[atom2].myGBSA_T->bornRad;
-      
-      scaledCharge_i = topo->atoms[atom1].scaledCharge;
-      scaledCharge_l = topo->atoms[atom2].scaledCharge;
-      
-      Real ril = sqrt(distSquared);
+    public:
+      enum {DIST_R2 = 1};
+      enum {CUTOFF = 0};
+    public:
+      GBPartialSum();
         
-      expterm = (ril*ril)/(4.0*bornRad_i*bornRad_l);
-      filGB = sqrt(ril*ril + bornRad_i*bornRad_l*exp(-expterm));
-        
-      topo->atoms[atom1].myGBSA_T->partialGBForceTerms += scaledCharge_i*scaledCharge_l*(1/(filGB*filGB))*0.5*(1/filGB)*exp(-expterm)*(bornRad_l + (ril*ril)/(4.0*bornRad_i));
+      void operator()(Real &energy, Real &force, Real distSquared, Real rDistSquared, 
+                      const Vector3D &diff, const GenericTopology *topo, int atom1, 
+                      int atom2, ExclusionClass excl) const;
       
-      topo->atoms[atom2].myGBSA_T->partialGBForceTerms += scaledCharge_i*scaledCharge_l*(1/(filGB*filGB))*0.5*(1/filGB)*exp(-expterm)*(bornRad_i + (ril*ril)/(4.0*bornRad_l));
-
-    }
+      static void accumulateEnergy(ScalarStructure *energies, Real energy);    
+      static Real getEnergy(const ScalarStructure *energies);      
     
-    static void accumulateEnergy(ScalarStructure *energies, Real energy) {
-    }
-    
-    static Real getEnergy(const ScalarStructure *energies) {
-      return 0;
-    }
-    
-    static void postProcess(const GenericTopology *topo, ScalarStructure *energies) {
-      const unsigned int atomnumber = topo->atoms.size();
+      static void postProcess(const GenericTopology *topo, ScalarStructure *energies);      
+      static void parallelPostProcess(const GenericTopology *topo, ScalarStructure *energies);
+      static bool doParallelPostProcess();      
       
-      for( unsigned int i=0; i<atomnumber; i++)
-        topo->atoms[i].myGBSA_T->havePartialGBForceTerms = true;
-    }
-    
-    static void parallelPostProcess(const GenericTopology *topo, ScalarStructure *energies) {
-      const unsigned int atomnumber = topo->atoms.size();
-      
-      // Copy Radii
-      Real *partial = new Real[ atomnumber ];
-      
-      //put radii into array
-      for( unsigned int i = 0; i < atomnumber; i++ ){
-        partial[i] = topo->atoms[i].myGBSA_T->partialGBForceTerms;
-      }
-      
-      //sum accross nodes
-      Parallel::reduce(partial, partial + atomnumber); //reduceSlaves only?
-      
-      //put radii back
-      for( unsigned int i = 0; i < atomnumber; i++ ){
-        topo->atoms[i].myGBSA_T->partialGBForceTerms = partial[i];
-      }
-      
-      delete [] partial;
-      
-    }
-    
-    //do parallel post process?
-    static bool doParallelPostProcess() {
-      return true;
-    }
-    
-    // Parsing
-    static std::string getId() {return keyword;}
-    static unsigned int getParameterSize() {return 0;}
-    void getParameters(std::vector<Parameter> &) const {}
-    
-    static GBPartialSum make(const std::vector<Value> &) {
-      return GBPartialSum();
-    }
-    
-    
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // My data members
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  public:
-    static const std::string keyword;
-    
+      // Parsing
+      static std::string getId();
+      static unsigned int getParameterSize();
+      void getParameters(std::vector<Parameter> &) const;
+      static GBPartialSum make(const std::vector<Value> &);
+    public:
+      static const std::string keyword;
   };
-  
 }
 
 #endif
